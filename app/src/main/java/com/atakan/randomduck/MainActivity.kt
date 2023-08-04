7b1319b7e73da52336890c1007d66d57828f7078
unknown <atakan.akin@metu.edu.tr>
2023-08-04 14:48:21 +0300

    Exception Handling

--- app/src/main/java/com/atakan/randomduck/MainActivity.kt
+++ app/src/main/java/com/atakan/randomduck/MainActivity.kt
@@ -2,6 +2,7 @@ package com.atakan.randomduck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.DrawableRes
 import com.atakan.randomduck.databinding.ActivityMainBinding
 import androidx.lifecycle.lifecycleScope
@@ -13,32 +14,43 @@ import kotlinx.coroutines.withContext
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var imageDuck: String? = null
-
+    val randDuck = RetrofitMain.getInstance().create(DuckApi::class.java)
+    var isCrash = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
-        val randDuck = RetrofitMain.getInstance().create(DuckApi::class.java)
-        lifecycleScope.launch{
-            var result = randDuck.getRandomDuck()
-            if(result != null){
-                imageDuck = result.body()?.url
-                withContext(Dispatchers.Main){
-                    Glide.with(this@MainActivity).load(imageDuck).into(binding.duckImg)
-                }
-            }
-        }
+        getTheDuck()
         binding.newBtn.setOnClickListener(){
-            lifecycleScope.launch{
+            getTheDuck()
+        }
+
+        setContentView(binding.root)
+    }
+
+    private fun getTheDuck(){
+        val duration = Toast.LENGTH_SHORT
+        lifecycleScope.launch{
+            try{
                 var result = randDuck.getRandomDuck()
                 if(result != null){
+                    if(isCrash){
+                        Toast.makeText(this@MainActivity, "Reconnecting.", duration).show()
+                    }
                     imageDuck = result.body()?.url
+                    println(imageDuck)
                     withContext(Dispatchers.Main){
                         Glide.with(this@MainActivity).load(imageDuck).into(binding.duckImg)
+                        isCrash = false
                     }
                 }
+                else{
+                    isCrash = true
+                    Toast.makeText(this@MainActivity, "Failed to fetch the duck. Check your internet connection.", duration).show()
+                }
+            }catch(e : Exception){
+                isCrash = true
+                Toast.makeText(this@MainActivity, "Failed to fetch the duck. Check your internet connection.", duration).show()
             }
         }
-        setContentView(binding.root)
     }
 }
